e0bf7d19ec6b1f8a0427007678c260242535d2c7
Sergalas <svbeloventcev@sberinsur.ru>
2026-02-10 16:18:01 +0300

    first commit

---
+++ src/main/java/ru/sergalas/amplicodebot/AmplicodeBotApplication.java
@@ -0,0 +1,13 @@
+package ru.sergalas.amplicodebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplicodeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmplicodeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sergalas/amplicodebot/AmplicodeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergalas.amplicodebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplicodeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
